128a381e516d049f121c7dee25e70f18436c57e8
Anuragkillswitch <70265851+Anuragkillswitch@users.noreply.github.com>
2022-01-24 13:32:49 +0530

    delete posts by id SQL Alchemy

--- app/main.py
+++ app/main.py
@@ -130,13 +130,18 @@ def get_post(id : int,db: Session = Depends(get_db)):
     return {"post_detail": post }
 
 @app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""",(id,))
-    post=cursor.fetchone()
-    conn.commit()
-    if post==None:
+def delete_post(id: int,db: Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""",(id,))
+    # post=cursor.fetchone()
+    # conn.commit()
+    post=db.query(models.Post).filter(models.Post.id==id)
+    
+    if post.first()==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=post)
+        
+    post.delete(synchronize_session=False)    
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)    
 
 
