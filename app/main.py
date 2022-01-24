8540bd7b4841fa32b40e3a8f88de6f777c81d249
Anuragkillswitch <70265851+Anuragkillswitch@users.noreply.github.com>
2022-01-24 10:00:43 +0530

    update posts by ID DB conn commit

--- app/main.py
+++ app/main.py
@@ -107,7 +107,7 @@ def update_post(id : int,post: Post):
     cursor.execute("""UPDATE posts SET title= %s,content=%s,published=%s WHERE id= %s RETURNING *""",(post.title,post.content,post.published,id))
     updated_post=cursor.fetchone()
     conn.commit()
-    if update_post==None:
+    if updated_post==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with id {id} does not exist")
       
