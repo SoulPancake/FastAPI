a45c5b69b13c06016f640dc5d2964a0e69d1dbbb
Anuragkillswitch <70265851+Anuragkillswitch@users.noreply.github.com>
2022-01-26 09:52:27 +0530

    after using OAuth2PasswordForm We need to pass the data in postman as a form not Raw

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter,Depends,status,HTTPException,Response
 from sqlalchemy.orm import Session
-
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 
 from .. import database,schema,models,utils,oauth2
@@ -9,8 +9,8 @@ router=APIRouter(tags=['Authentication'])
 
 
 @router.post("/login")
-def login(user_credentials : schema.UserLogin,db:Session=Depends(database.get_db)):
-    user=db.query(models.User).filter(models.User.email==user_credentials.email).first()
+def login(user_credentials : OAuth2PasswordRequestForm=Depends(),db:Session=Depends(database.get_db)):
+    user=db.query(models.User).filter(models.User.email==user_credentials.username).first()
     if not user:
         raise HTTPException(status_code=404,detail="Invalid credentials : User doesn't exist")
     
